c4306d26cc17843feb4b1d5199e9276d80bc1b23
fj81701 <fj81701@gmail.com>
2024-11-07 11:02:35 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Q.Tools
+{
+    public class SceneControl : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            // 場景管理器 的 載入場景(場景名稱)
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void QuitGame()
+        {
+            //應用程式的離開();
+            Application.Quit();
+        }
+    }
+}
